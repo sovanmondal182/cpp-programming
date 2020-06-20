b74a7ec491077bfb86e5b632ee6868ccc690f12f
sovanmondal182 <56794254+sovanmondal182@users.noreply.github.com>
2020-06-20 23:48:41 +0530

    Update product.cpp

--- product.cpp
+++ product.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     double i,j,k;
     cout << "Enter two numbers: ";
-	cin >> i >> j;
+    cin >> i >> j;
     k=i*j;  
     cout << "Product = " << k;    
-	return 0;
+    return 0;
 }
