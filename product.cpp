3d218dc098ea3cdcf6b87fbdf374a10a9c506b34
sovanmondal182 <56794254+sovanmondal182@users.noreply.github.com>
2020-06-20 23:47:53 +0530

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double i,j,k;
+    cout << "Enter two numbers: ";
+	cin >> i >> j;
+    k=i*j;  
+    cout << "Product = " << k;    
+	return 0;
+}
